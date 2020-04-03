36a29aeee30b889e3e98028bf511b4d4af2b7add
Ryan Duff <ryanduff613@gmail.com>
2020-04-03 16:04:46 -0700

    add function to reverse list below previously printed list, all code working

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,9 @@
 // Business Logic
 var returnList = function(userNumber){
-  
   var numbersStrings = [];
     for (var index = 1; index <= userNumber; index+=1){
       numbersStrings.push(index.toString());
   }
-  
   var beeps = numbersStrings.map(function(string){
     if (string.includes('23')) {
       return "Jordan";
@@ -20,10 +18,31 @@ var returnList = function(userNumber){
     } else {
       return string;
     }
-
   });
-  console.log(beeps);
   return beeps.join(" ");
+};
+
+var reverseList = function(userNumber){
+  var numbersStrings = [];
+    for (var index = 1; index <= userNumber; index+=1){
+      numbersStrings.push(index.toString());
+  }
+  var beeps = numbersStrings.map(function(string){
+    if (string.includes('23')) {
+      return "Jordan";
+    }else if (string.includes('3')){
+      return "Won't you be my neighbor?";
+    }else if (string.includes('2')){
+      return "Boop";
+    }else if (string.includes('1')) {
+      return "Beep";
+    } else if (string.includes('49')) {
+      return "49ers are my favorite team"
+    } else {
+      return string;
+    }
+  });
+  return beeps.reverse().join(" ");
 };
 
 
@@ -37,5 +56,11 @@ $(document).ready(function() {
     $('#output').text(output);
     $("#outputBox").show();
   });
+  $('#reverse').submit(function(event){
+    event.preventDefault();
+    var userNumber = parseInt($('#userInput').val());
+    var reverseOutput = reverseList(userNumber);
+    $('#reversed').text(reverseOutput);
+  })
 
 });
