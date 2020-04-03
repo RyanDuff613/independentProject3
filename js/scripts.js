e210f203ddf12d7417bf1b08f5c05f8efe1601df
Ryan Duff <ryanduff613@gmail.com>
2020-04-03 10:37:51 -0700

    add initial html form and js returning user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+// Business Logic
+
+// UI Logic
 $(document).ready(function() {
+  $('#theForm').submit(function(event){
+    event.preventDefault;
+    var userNumber = $('userNumber').val();
+    console.log(userNumber);
+  });
 
 });
