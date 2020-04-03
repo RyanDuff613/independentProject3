725038aab5bbe75ec51767323d371c5c07396634
Ryan Duff <ryanduff613@gmail.com>
2020-04-03 13:52:54 -0700

    refactor code to eliminate unecessary creation of intermediate array

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
 // Business Logic
 var returnList = function(userNumber){
-  var countingNumbers = [];
-  for (var index = 1; index <= userNumber; index+=1){
-    countingNumbers.push(index);
-  }
-  var beeps = countingNumbers.map(function(number){
-    return number.toString();
-  });
   
-
-  var boops = beeps.map(function(beep){
-    if (beep.includes('3')){
+  var numbersStrings = [];
+    for (var index = 1; index <= userNumber; index+=1){
+      numbersStrings.push(index.toString());
+  }
+  console.log(numbersStrings);
+  
+  var beeps = numbersStrings.map(function(string){
+    if (string.includes('3')){
       return "Won't you be my neighbor?";
-    }else if (beep.includes('2')){
+    }else if (string.includes('2')){
       return "Boop";
-    }else if (beep.includes('1')) {
-      return beep = "Beep";
+    }else if (string.includes('1')) {
+      return "Beep";
     }else {
-      return beep;
+      return string;
     }
   });
-  return boops;
+  return beeps;
 };
 
 
