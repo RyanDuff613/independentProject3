8ea55f221fa40ff2d2388a179844c71d03e3175e
Ryan Duff <ryanduff613@gmail.com>
2020-04-03 11:37:40 -0700

    create array to hold list of numbers, create for loop that pushes ascendingn list of numbers into array

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
 // Business Logic
+var returnList = function(userNumber){
+  var countingNumbers = [];
+  for (var index = 1; index <= userNumber; index+=1){
+    countingNumbers.push(index);
+  }
+    console.log(countingNumbers);
+    console.log(index);
+    return countingNumbers;
+};
+
+
+
+
 
 // UI Logic
 $(document).ready(function() {
   $('#theForm').submit(function(event){
-    var userNumber = $('#userInput').val();
-    var output = myFunction(userNumber);
-    console.log(userNumber);
     event.preventDefault();
+    var userNumber = $('#userInput').val();
+    var output = returnList(userNumber);
+    console.log(userNumber);
+    console.log(output);
+    
   });
 
 });
