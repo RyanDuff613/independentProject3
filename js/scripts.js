3eb23e78cadcf7ec4c2432b197dd135ffa4bccde
Ryan Duff <ryanduff613@gmail.com>
2020-04-03 16:50:18 -0700

    add feedback for user if they fail to input numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,37 @@
 // Business Logic
 var returnList = function(userNumber, name){
-    var numbersStrings = [];
-    for (var index = 1; index <= userNumber; index+=1){
-        numbersStrings.push(index.toString());
-    }
-    var beeps = numbersStrings.map(function(string){
-      if (string.includes('23')) {
-        return "Jordan";
-      }else if (string.includes('3')){
-        return "Won't you be my neighbor?";
-      }else if (string.includes('2')){
-        return "Boop";
-      }else if (string.includes('1')) {
-        return "Beep";
-      }else if (userNumber % 3 === 0){
-        return "Won't you be my neighbor," + name;
-      }else if (string.includes('49')) {
-        return "49ers are my favorite team"
-      }else {
-        return string;
+    if (isNaN(userNumber)){
+      return "Sorry, I only accept whole numbers."
+    }else {
+      var numbersStrings = [];
+      for (var index = 1; index <= userNumber; index+=1){
+          numbersStrings.push(index.toString());
       }
-    });
-  return beeps.join(" "); 
+      var beeps = numbersStrings.map(function(string){
+        if (string.includes('23')) {
+          return "Jordan";
+        }else if (string.includes('3')){
+          return "Won't you be my neighbor?";
+        }else if (string.includes('2')){
+          return "Boop";
+        }else if (string.includes('1')) {
+          return "Beep";
+        }else if (userNumber % 3 === 0){
+          return "Won't you be my neighbor," + name;
+        }else if (string.includes('49')) {
+          return "49ers are my favorite team"
+        }else {
+          return string;
+        }
+      });
+    return beeps.join(" "); 
+  } 
 };
 
-var reverseList = function(userNumber){
+
+var reverseList = function(userNumber, name){
   var numbersStrings = [];
-    for (var index = 1; index <= userNumber; index+=1){
+  for (var index = 1; index <= userNumber; index+=1){
       numbersStrings.push(index.toString());
   }
   var beeps = numbersStrings.map(function(string){
@@ -38,7 +43,9 @@ var reverseList = function(userNumber){
       return "Boop";
     }else if (string.includes('1')) {
       return "Beep";
-    } else if (string.includes('49')) {
+    }else if (userNumber % 3 === 0){
+      return "Won't you be my neighbor," + name;
+    }else if (string.includes('49')) {
       return "49ers are my favorite team"
     } else {
       return string;
@@ -46,9 +53,6 @@ var reverseList = function(userNumber){
   });
   return beeps.reverse().join(" ");
 };
-
-
-
 
 // UI Logic
 $(document).ready(function() {
@@ -66,5 +70,4 @@ $(document).ready(function() {
     var reverseOutput = reverseList(userNumber);
     $('#reversed').text(reverseOutput);
   })
-
 });
