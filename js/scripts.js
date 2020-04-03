11ff6c97082e9825e9c6c7d5ca7e4a6bd6eeb796
Ryan Duff <ryanduff613@gmail.com>
2020-04-03 10:45:57 -0700

    page properly returns user input

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,9 @@
 // UI Logic
 $(document).ready(function() {
   $('#theForm').submit(function(event){
-    event.preventDefault;
-    var userNumber = $('userNumber').val();
+    var userNumber = $('#userInput').val();
     console.log(userNumber);
+    event.preventDefault();
   });
 
 });
